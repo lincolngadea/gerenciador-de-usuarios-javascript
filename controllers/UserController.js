6145ef0582dd4ec2b77267a5284a42188bbffa95
Lincoln Gadea <lincoln@glinc.com.br>
2021-01-03 12:39:36 -0300

    chore: implementa o painel de contagem para usuarios e admins

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -18,6 +18,8 @@ class UserController{
 
       let values = this.getValues()
 
+      if(!values) return false;
+
       this.getPhotos().then((content)=>{
         values.photo = content;
                   
@@ -118,6 +120,8 @@ class UserController{
 
     let tr = document.createElement('tr');
 
+    tr.dataset.user = JSON.stringify(dataUser);
+
     tr.innerHTML =
       `
         <td><img src=${dataUser.photo} alt="User Image" class="img-circle img-sm"></td>
@@ -132,6 +136,26 @@ class UserController{
       `
 
   this.tableEl.appendChild(tr);
+
+  this.updateCount();
   
+  }
+
+  updateCount(){
+
+    let numberUsers = 0;
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach(tr=>{
+      numberUsers++;
+
+      let user = JSON.parse(tr.dataset.user);
+
+      if(user._admin) numberAdmin++;
+    })
+
+    document.querySelector("#number-users").innerHTML = numberUsers;
+    document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+
   }
 }
