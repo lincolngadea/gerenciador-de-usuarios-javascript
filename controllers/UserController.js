4ff276a81e4ee469cd4b2b0d601d3a711ec33107
Lincoln Gadea <lincoln@glinc.com.br>
2021-01-05 10:44:52 -0300

    chore: altera dados do objeto

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,18 +1,57 @@
 class UserController {
-  constructor(formId, tableId) {
-    this.formEl = document.getElementById(formId);
+  constructor(formIdCreate,formIdUpdate, tableId) {
+    this.formEl = document.getElementById(formIdCreate);
+    this.formUpdateEl = document.getElementById(formIdUpdate);
     this.tableEl = document.getElementById(tableId);
 
     this.onSubmit();
-    this.onEditCancel();
+    this.onEdit();
   }
 
-  onEditCancel() {
+  onEdit() {
     document
       .querySelector("#box-user-update .btn-cancel")
       .addEventListener("click", (e) => {
         this.showPanelCreate();
       });
+
+    this.formUpdateEl.addEventListener("submit", event=>{
+      event.preventDefault();
+
+      let btn = this.formUpdateEl.querySelector("[type=submit]");
+
+      btn.disabled = true;
+
+      let values = this.getValues(this.formUpdateEl);
+
+      console.log(values)
+
+      let index = this.formUpdateEl.dataset.trIndex;
+
+      let tr = this.tableEl.rows[index];
+
+      tr.dataset.user = JSON.stringify(values);
+
+      tr.innerHTML = `
+          <td><img src=${
+            values.photo
+          } alt="User Image" class="img-circle img-sm"></td>
+          <td>${values.name}</td>
+          <td>${values.email}</td>
+          <td>${values.admin}</td>
+          <td>${Utils.dateFormat(values.register)}</td>
+          <td>
+            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+          </td>
+      `;
+
+      this.addEventsTr(tr);
+
+      this.updateCount();
+
+    })
+    
   }
 
   onSubmit() {
@@ -23,7 +62,7 @@ class UserController {
 
       btn.disabled = true;
 
-      let values = this.getValues();
+      let values = this.getValues(this.formEl);
 
       if (!values) return false;
 
@@ -46,13 +85,13 @@ class UserController {
     });
   }
 
-  getValues() {
+  getValues(formEl) {
     let user = {};
     let isValid = true;
 
-    [...this.formEl.elements].forEach((field, index) => {
+    [...formEl.elements].forEach((field, index) => {
       if (
-        ["name", "email", "password"].indexOf(field.name) >= 0 &&
+        ["name", "email","password"].indexOf(field.name) >= 0 &&
         !field.value
       ) {
         field.parentElement.classList.add("has-error");
@@ -65,7 +104,8 @@ class UserController {
           // console.log(field)
         } 
       } else if (field.name == "admin") {
-        user[field.name] = field.checked ? "SIM" : "NÃO";
+        user[field.name] = field.checked;
+        console.log(user[field.name]);
       } else {
         user[field.name] = field.value;
       }
@@ -126,7 +166,7 @@ class UserController {
         } alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
+        <td>${dataUser.admin ? "SIM" : "NÃO"}</td>
         <td>${Utils.dateFormat(dataUser.register)}</td>
         <td>
           <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
@@ -134,14 +174,23 @@ class UserController {
         </td>
       `;
 
+    this.addEventsTr(tr);
+        
+    this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+
+  addEventsTr(tr){
     tr.querySelector(".btn-edit").addEventListener("click",e=> {
       let json = JSON.parse(tr.dataset.user);
       let form = document.querySelector("#form-user-update");
       // console.log("Form:",json)
+      form.dataset.trIndex = tr.sectionRowIndex;
       for (let name in json) {
         let field = form.querySelector("[name="+name.replace("_", "")+"]");
         
-
+        
         if (field) {
           
           switch(field.type){
@@ -167,9 +216,6 @@ class UserController {
 
       this.showPanelUpdate();
     });
-    this.tableEl.appendChild(tr);
-
-    this.updateCount();
   }
 
   showPanelCreate() {
